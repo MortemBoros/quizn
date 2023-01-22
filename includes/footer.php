f21a90ac925848261f802d83ec59607384391d1c
MortemBoros <122373894+MortemBoros@users.noreply.github.com>
2023-01-22 06:02:40 +0300

    Delete includes directory

--- includes/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="footer">
-                  <p>QuizNight &copy; <?php echo date('Y'); ?></p>
-            </div>
-      </div>
-      <!-- // container -->
-</body>
-</html>
--- includes/head_section.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-<!-- Google Fonts -->
-<link href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine" rel="stylesheet">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-<!-- Styling for public area -->
-<link rel="stylesheet" href="static/css/styling.css">
-<meta charset="UTF-8">
